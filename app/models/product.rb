baa3f080607504f698a031ecc61b001060649674
NerminMajedMahdi <mahdi.nirmeen@gmail.com>
2017-11-26 20:12:32 +0200

    5.9 pagentaion and validation models

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,6 +4,7 @@ class Product < ApplicationRecord
 
   validates :name, :description, :image_url, :colour, :price, presence: true
 
+
   def self.search(search_term)
     Product.where("name LIKE ?", "%#{search_term}%")
   end
