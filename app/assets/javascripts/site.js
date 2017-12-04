4d70935572a5e334751d637613e2030fc1ba8503
NerminMajedMahdi <mahdi.nirmeen@gmail.com>
2017-12-04 09:40:48 +0200

    6.12 mime cashing and keys

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,15 +1,15 @@
 function refreshRating() {
   $('.rating').raty({
-    starHalf: '<%= asset_path 'star-half.png' %>',
-    starOff: '<%= asset_path 'star-off.png' %>',
+    starHalf: '<%= asset_path "star-half.png" %>',
+    starOff: '<%= asset_path "star-off.png" %>',
     starOn: '<%= asset_path 'star-on.png' %>',
     scoreName: 'comment[rating]'
   });
 
   $('.rated').raty({
-    starHalf: '<%= asset_path 'star-half.png' %>',
-    starOff: '<%= asset_path 'star-off.png' %>',
-    starOn: '<%= asset_path 'star-on.png' %>',
+    starHalf: '<%= asset_path "star-half.png" %>',
+    starOff: '<%= asset_path "star-off.png" %>',
+    starOn: '<%= asset_path "star-on.png" %>',
     readOnly: true,
     score: function() {
       return $(this).attr('data-score');
