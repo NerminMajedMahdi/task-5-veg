d1738c57e52ef449d010a1493717a11f7acb2184
NerminMajedMahdi <mahdi.nirmeen@gmail.com>
2017-12-05 07:53:58 +0200

    6.9 and 6.10

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,9 @@ require "action_cable/engine"
 require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
+
+
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -22,7 +25,12 @@ module Trails1
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 config.cache_store = :redis_store, "redis://localhost:6379/0/cache", { expires_in: 90.minutes }
+config.cache_store = :dalli_store
   end
 end
+
+
+
+
 
 
