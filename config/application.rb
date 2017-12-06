68541b6ce502e14a2a28ccfebe52b333b05ee896
NerminMajedMahdi <mahdi.nirmeen@gmail.com>
2017-12-06 03:43:21 +0200

    6.9

--- config/application.rb
+++ config/application.rb
@@ -24,10 +24,14 @@ module Trails1
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-
   end
 end
 
+module YourApp
+  class Application < Rails::Application
+    config.assets.paths << Rails.root.join('node_modules')
+ end
+end
 
 
 
