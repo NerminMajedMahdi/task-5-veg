41dee7c67fcc8610410a86659dec89c49887d1ec
NerminMajedMahdi <mahdi.nirmeen@gmail.com>
2018-01-15 11:16:00 +0200

    6.15 final project with more test for product controller

--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -11,7 +11,65 @@ describe ProductsController, type: :controller do
     end
   end
 
+  context "GET #show:" do
+    let(:product) { FactoryBot.create(:product) }
+    it "renders products show template" do
+      get :show, params: { id: product.id }
+      expect(response).to be_ok
+      expect(response).to render_template('show')
+    end
+  end
 
+  context "GET #new:" do
+    let(:product) { FactoryBot.create(:product) }
+    let(:user) { FactoryBot.create(:user) }
+    let(:admin) { FactoryBot.create(:admin) }
+
+    it "user login - can not add new products" do
+      sign_in user
+      get :new
+      expect(flash[:alert]).to eq('You are not authorized to access this page.')
+      expect(response).to redirect_to root_url
+    end
+
+    it "admin login - can add new products" do
+      sign_in admin
+      get :new
+      expect(response).to be_ok
+      expect(response).to render_template('new')
+    end
+  end
+
+  context "POST #create:" do
+    let(:user) { FactoryBot.create(:user) }
+    let(:admin) { FactoryBot.create(:admin) }
+
+    it "admin login - can create new product" do
+      sign_in admin
+      post :create, params: {product: {name:"Dining Set", description:'description', price_in_cents:'55', colour:'pink',image_url:'yy'}}
+      expect(response).to redirect_to product_path(Product.last.id)
+    end
+  end
+
+  context "GET #edit" do
+    let(:product) { FactoryBot.create(:product) }
+    let(:user) { FactoryBot.create(:user) }
+    let(:admin) { FactoryBot.create(:admin) }
+
+    it "admin login - can edit products" do
+      sign_in admin
+      get :edit, params: {id: product.id} 
+      expect(response).to be_ok
+      expect(response).to render_template('edit')
+    end
+
+    it "user login - can not edit products" do
+      sign_in user
+      get :edit, params: {id: product.id} 
+      expect(flash[:alert]).to eq('You are not authorized to access this page.')
+      expect(response).to redirect_to root_url
+    end
+  end
 
   context "PUT #update" do
     let(:product) { FactoryBot.create(:product) }
@@ -25,6 +83,24 @@ describe ProductsController, type: :controller do
     end
   end
 
- 
+  context "DELETE #destroy:" do
+    let(:product) { FactoryBot.create(:product) }
+    let(:user) { FactoryBot.create(:user) }
+    let(:admin) { FactoryBot.create(:admin) }
+
+    it "user login - can not delete products" do
+      sign_in user
+      delete :destroy, params: { id: product.id }
+      expect(flash[:alert]).to eq('You are not authorized to access this page.')
+      expect(response).to redirect_to root_url
+    end
+
+    it "admin login - can delete products" do
+      sign_in admin
+      delete :destroy, params: {id: product.id}
+      flash[:notice].should eql("Product was successfully destroyed.")
+      expect(response).to redirect_to products_path
+    end
+  end
 
 end
